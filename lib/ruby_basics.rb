d24faf27ebc2d451bbd3f8f3a9be3d866a9a3f69
BriOD <briodonoghue@gmail.com>
2016-02-26 11:45:03 -0500

    todo ruby basics lab

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,35 @@
-def division(num1, num2)
- 
+def division(num1 = 6, num2 = 1)
+    num1/num2
 end
 
-def assign_variable(value)
-  
+
+
+def assign_variable(value = Bob)
+  value
 end
 
-def argue
-  
+def argue(name)
+  name
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
+    "Nice"
 
 end
+
+
+
 
 def last_evaluated_value
+    "expert"
 
 end
 
-def pizza_party
+def pizza_party(cheese = "cheese")
+    cheese
 
 end
